55787e9ed35489b41afa8bceff68205a7af25e47
William Truong <williamtruong@Williams-MacBook-Pro-2.local>
2017-04-05 22:06:27 -0400

    added function that returns test data from database

--- python/ml/code/ml.py
+++ python/ml/code/ml.py
@@ -20,7 +20,30 @@ def load_songs():
                     SELECT spotify_song_popularity, acousticness, danceability, energy,
                     instrumentalness, loudness, mode, speechiness, tempo, valence,
                     liveness
-                    FROM songs
+                    FROM songs ORDER BY song_name LIMIT 1000
+                '''):
+
+        songs_features.append(song[1:])
+        songs_popularity.append(song[0])
+
+    return songs_features, songs_popularity
+
+# Same function as above, but loads the next 10 songs after the initial 1000 songs
+# we trained on.
+def load_test_songs():
+    ### MODIFY THIS TO POINT TO YOUR DATABASE ###
+    conn = sqlite3.connect('../../../node/app/database.db')
+    conn.text_factory = str
+    c = conn.cursor()
+
+    songs_features = []
+    songs_popularity = []
+
+    for song in c.execute('''
+                    SELECT spotify_song_popularity, acousticness, danceability, energy,
+                    instrumentalness, loudness, mode, speechiness, tempo, valence,
+                    liveness
+                    FROM songs ORDER BY song_name LIMIT 10 OFFSET 1000
                 '''):
 
         songs_features.append(song[1:])
@@ -30,3 +53,6 @@ def load_songs():
 
 if __name__ == '__main__':
     songs_features, songs_popularity = load_songs()
+
+    test_features, test_popularity = load_test_songs()
+    print(test_popularity)
