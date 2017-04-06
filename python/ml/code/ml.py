d1ff363d534da30d313e154213084c9820b95bec
William Truong <williamtruong@Williams-MacBook-Pro-2.local>
2017-04-05 21:45:39 -0400

    split array into features and labels

--- python/ml/code/ml.py
+++ python/ml/code/ml.py
@@ -3,25 +3,30 @@ import sqlite3
 import csv
 import sys
 
-# Load songs into an array and returns it (data structure consists of
+# Load songs into two arrays and returns them (first array consists of
 # an array of tuples, where each tuple represents the features of a
-# particular song).
+# particular song. second array consists of popularity for a song. indexing
+# matches for both arrays for each song).
 def load_songs():
     ### MODIFY THIS TO POINT TO YOUR DATABASE ###
     conn = sqlite3.connect('../../../node/app/database.db')
     conn.text_factory = str
     c = conn.cursor()
-    songs = []
+
+    songs_features = []
+    songs_popularity = []
+
     for song in c.execute('''
                     SELECT spotify_song_popularity, acousticness, danceability, energy,
                     instrumentalness, loudness, mode, speechiness, tempo, valence,
                     liveness
                     FROM songs
                 '''):
-        songs.append(song)
 
-    return songs
+        songs_features.append(song[1:])
+        songs_popularity.append(song[0])
+
+    return songs_features, songs_popularity
 
 if __name__ == '__main__':
-    list_of_songs = load_songs()
-    print(list_of_songs)
+    songs_features, songs_popularity = load_songs()
