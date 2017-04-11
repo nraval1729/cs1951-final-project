d1c000b97c070bf7ef712d675c2fd6339523ee91
Andrew Osgood <andrew_osgood@brown.edu>
2017-04-11 17:23:43 -0400

    extract jams added but bugged

--- python/extract_jams.py
+++ python/extract_jams.py
@@ -2,11 +2,17 @@
 import sqlite3
 import csv
 import sys
-from unidecode import unidecode
+import unicodedata
+#from unidecode import unidecode
 
-reload(sys)
-sys.setdefaultencoding("utf-8")
+#tsv jams: 2.095
+#1479780
+
 csv.field_size_limit(sys.maxsize)
+
+def strip_accents(s):
+   return ''.join(c for c in unicodedata.normalize('NFD', s)
+                  if unicodedata.category(c) != 'Mn')
 
 def load():
 
@@ -27,28 +33,28 @@ def load():
 
 
     with open('../data/archive/jams.tsv','rU') as tsvin:
+        with open('../data/archive/jams.tsv','rU') as tsvin2:
             reader = csv.DictReader(tsvin, delimiter='\t')
             for row in reader:
                 spotify_uri = row["spotify_uri"]
-                if (not row["spotify_uri"] and row[]):
-                    for row2 in reader:
-                        artist1 = unidecode(row["artist"])
-                        artist2 = unidecode(row2["artist"])
-                        song = unidecode(row["title"])
-                        song2 = unidecode(row2["title"])
-                
-                        if (row2["spotify_uri"] and (song == song2) and (artist == artist2)):
-                            spotify_uri = row2["spotify_uri"]
-
-                        if row["artist"] != artist1:
-                            print (artist1)
-                            print (row["artist"])
-                            print (spotify_uri)
+                if (not row["spotify_uri"] and row["title"] and row["artist"]):
+                    tsvin2.seek(0)
+                    reader2 = csv.DictReader(tsvin2, delimiter='\t')
+                    for row2 in reader2:
+                        if (row2["artist"] and row2["title"]):
+                            artist1 = strip_accents((row["artist"]))
+                            artist2 = strip_accents((row2["artist"]))
+                            song = strip_accents((row["title"]))
+                            song2 = strip_accents((row2["title"]))
+                    
+                            if (row2["spotify_uri"] and (song == song2) and (artist1 == artist2)):
+                                spotify_uri = row2["spotify_uri"]
+                print (spotify_uri)
                 if (spotify_uri):
                     c.execute('''
                         INSERT OR IGNORE INTO jams
                         VALUES (?, ?, ?)
-                        ''', (row["jam_id"], spotify_uri[14:], row["creation_date"]))
+                        ''', (row["jam_id"], spotify_uri[14:].encode("utf-8"), row["creation_date"]))
     conn.commit()
     conn.close()
 
