3aea5f71a8e6e737f932203908b51221219cce32
youngrrrr <young-rae_kim@brown.edu>
2017-05-09 15:01:06 -0400

    Changes so far to classifier controller

--- webapp/app/classifier/controller.js
+++ webapp/app/classifier/controller.js
@@ -11,8 +11,12 @@ var db = new sqlite3.Database(dbPath, sqlite3.OPEN_READONLY);
 module.exports = {
   classifyNewSongPost: classifyNewSongPost,
   exploreSpotterPost: Promise.coroutine(exploreSpotterGenerator),
-  featureAveragesGet: Promise.coroutine(featureAveragesGet)
+  featureAveragesGet: featureAveragesGet
 }
+
+/////////////////////
+// Route Functions //
+/////////////////////
 
 // Given the raw feature values for a random Spotify song, classify its
 // popularity by our classification algorithm
@@ -27,17 +31,15 @@ function classifyNewSongPost(req, res) {
   newSongFeatures.tempo = req.body.tempo;
   newSongFeatures.time_signature = req.body.timeSignature;
 
-  // The actual stringified input to the Python ML Process
-  var featuresInputToPythonProcess = JSON.stringify(newSongFeatures);
+  var songIdToFeaturesObj = {};
+  songIdToFeaturesObj[songId] = newSongFeatures;
 
-  /* SPAWN PROCESS HERE: START */
-  // Spawn the process here using the above stringified value as input.
-  // Assign the output to variable 'classifierPopularity', as below:
-  var classifierPopularity = 0;
-  /* SPAWN PROCESS HERE: END */
+  // Input must be in array
+  var predictedSongPopularity = classifyFeatures(songIdToFeaturesObj);
+  predictedSongPopularity = predictedSongPopularity[0];
 
   // Return this single value to the client
-  res.status(200).send(classifierPopularity);
+  res.status(200).send(predictedSongPopularity);
 }
 
 function* exploreSpotterGenerator(req, res) {
@@ -61,7 +63,67 @@ function* exploreSpotterGenerator(req, res) {
 
   var results = yield* runSqlQuery(getNRandomRowsQuery, getNRandomRowsParams);
 
-  res.status(200).send(results);
+  var songIdToFeaturesArray = [];  
+  for (var i = 0; i < results.length; i++) {
+    var songId = results[i]['song_id'];
+    delete(results[i]['song_id']);
+
+    var songIdToFeatures = {};
+    songIdToFeatures[songId] = results[i];
+
+    songIdToFeaturesArray.push(songIdToFeatures);
+  }
+
+  var predictedPopularityArray = classifyFeatures(songIdToFeaturesArray);
+
+  for (var songId in predictedPopularityArray) {
+
+  }
+
+  res.status(200).send();
+}
+
+// Get the popular and unpopular averages for all classifier features
+function featureAveragesGet(req, res) {
+  /* SPAWN PROCESS HERE: START */
+
+  // The output from the Python process
+  // Will be the stringified array of 2 JSON objects. The 0-index object will be the popular feature averages and the 1-index object will be the unpopular feature averages. Or vice-versa. Order doesn't really matter, just make sure it's documented.
+  var featureAveragesStr = '{"popular": {"avg_song_hotness":0.4541682036148893,"avg_artist_hotness":0.4171302102545193,"avg_duration":247.54609482734062,"avg_key":5.33493175949834,"avg_loudness":-9.42896972448431,"avg_mode":0.6634508980506767,"avg_tempo":125.48716993445494,"avg_time_signature":3.6235848253553895}, "unpopular": {"avg_song_hotness":0.4541682036148893,"avg_artist_hotness":0.4171302102545193,"avg_duration":247.54609482734062,"avg_key":5.33493175949834,"avg_loudness":-9.42896972448431,"avg_mode":0.6634508980506767,"avg_tempo":125.48716993445494,"avg_time_signature":3.6235848253553895}}';
+
+  // An important side note: make sure keys in the JSON string are surrounded by double-quote literals, else JSON.parse() will throw an error
+
+  /* SPAWN PROCESS HERE: END */
+
+  var featureAverages = JSON.parse(featureAveragesStr);
+
+  res.status(200).send(featureAverages);
+}
+
+//////////////////////
+// Helper Functions //
+//////////////////////
+
+// songIdToFeaturesObj: A map/object/dictionary where the keys are the song id
+// and the values are the corresponding features for said song, also an object
+// which maps the feature name to its corresponding raw value
+function classifyFeatures(songIdToFeaturesObj) {
+  // The actual stringified input to the Python ML Process
+  var songIdToFeaturesObjStr = JSON.stringify(songIdToFeaturesObj);
+
+  /* SPAWN PROCESS HERE: START */
+
+
+
+  // Spawn the process here using the above stringified value as input.
+  // Assign the output to variable 'classifierPopularity', as below:
+  var songIdToPredictedPopularityObj = {'song_id_1': 92.229, 'song_id_2': 28.393};
+
+
+
+  /* SPAWN PROCESS HERE: END */
+
+  return songIdToPredictedPopularityObj;
 }
 
 function* runSqlQuery(query, params) {
@@ -80,49 +142,4 @@ function sqlResults(statement, params) {
       }
     });
   });
-}
-
-// Get the popular and unpopular averages for all classifier features
-function* featureAveragesGet(req, res) {
-  var actualAverages = yield* getActualAverages();
-  actualAverages = actualAverages[0]; // what we want is nested in another array
-
-  var predictedAverages = getPredictedAverages();
-
-  res.status(200).send([actualAverages, predictedAverages]);
-}
-
-function* getActualAverages() {
-  var getActualAveragesQuery =
-    'SELECT AVG(song_hotness) AS avg_song_hotness, \
-            AVG(artist_hotness) AS avg_artist_hotness, \
-            AVG(duration) AS avg_duration, \
-            AVG(key) AS avg_key, \
-            AVG(loudness) AS avg_loudness, \
-            AVG(mode) AS avg_mode, \
-            AVG(tempo) AS avg_tempo, \
-            AVG(time_signature) AS avg_time_signature \
-     FROM songs;';
-
-  return yield* runSqlQuery(getActualAveragesQuery);
-}
-
-function getPredictedAverages() {
-  /* SPAWN PROCESS HERE: START */
-
-
-
-  // The output from the Python process
-  // Will be the stringified array of 2 JSON objects. The 0-index object will be the popular feature averages and the 1-index object will be the unpopular feature averages. Or vice-versa. Order doesn't really matter, just make sure it's documented.
-  var predictedAveragesStr = '{"avg_song_hotness":0.4541682036148893,"avg_artist_hotness":0.4171302102545193,"avg_duration":247.54609482734062,"avg_key":5.33493175949834,"avg_loudness":-9.42896972448431,"avg_mode":0.6634508980506767,"avg_tempo":125.48716993445494,"avg_time_signature":3.6235848253553895}';
-
-  // An important side note: make sure keys in the JSON string are surrounded by double-quote literals, else JSON.parse() will throw an error
-
-
-
-  /* SPAWN PROCESS HERE: END */
-
-  var predictedAveragesJson = JSON.parse(predictedAveragesStr);
-
-  return predictedAveragesJson;
 }
